a348a838325f5ee1f05ab5ad1bd25a5948588fb2
Aparanjith25 <karthiraja2507@gmail.com>
2025-10-04 23:25:23 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,13 +1,12 @@
 import gymnasium as gym
 
-# Breakout environment
 env = gym.make("Breakout-v5", render_mode="human")
-
 obs = env.reset()
 done = False
 
 while not done:
-    action = env.action_space.sample()  # random action for now
+    action = env.action_space.sample()  # random action
     obs, reward, done, truncated, info = env.step(action)
+    print("Reward:", reward)  # console lo reward kanipistundi
 
 env.close()
